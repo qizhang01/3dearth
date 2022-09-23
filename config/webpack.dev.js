d20cfe32ffeae7e766bee1abb1e188774878a155
qizhang <zhishui120@sina.com>
2022-09-23 12:57:48 +0800

    增加load air titles

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -44,6 +44,28 @@ const devWebpackConfig = merge.smart(baseWebpackConfig, {
     devServer: {
         ...config.devServer
     },
+    optimization: {
+        // 我们去查看哪些导出的模块被使用，然后再进行打包
+        usedExports: true,
+        // webpack默认会把Cesium和整个程序代码打进一个 chunk中，最终这个库非常庞大。
+        // 我们也可以用CommonChunksPlugin把Cesium打到它自己的包，提升程序的性能。
+        // 如果最终你的程序创建了多个chunks，他们可以引用一个通用的cesiumchunk。
+        // 最新版webpack使用config.optimization.splitChunks去设置
+        splitChunks: {
+            maxInitialRequests: Infinity,
+            minSize: 30000,
+            maxSize: 500000,
+            cacheGroups: {
+                commons: {
+                    name: 'Cesium',
+                    test: /[\\/]node_modules[\\/]cesium/,
+                    priority: 10,
+                    minChunks:2,
+                    chunks: 'initial'
+                }
+            }
+        }
+    }
 });
 
 module.exports = devWebpackConfig
--- src/pages/page1/vrbuilding/index.tsx
+++ src/pages/page1/vrbuilding/index.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
-import { Button } from 'antd'
+import { Select } from 'antd'
 import { Panel } from '@/components/Panel'
 import * as Cesium from 'cesium'
-
+import './index.css'
+const { Option } = Select
 let viewer: any = null
+let osmBuildingsTileset: any = null
 
 const Vrbuilding: React.FC = () => {
     React.useEffect(() => {
@@ -11,13 +13,26 @@ const Vrbuilding: React.FC = () => {
     }, [])
 
     const init3dViewer = () => {
-        viewer = new Cesium.CesiumWidget('buildingContainer', {
+        viewer = new Cesium.Viewer('buildingContainer', {
+            homeButton: false,
+            animation: false, //是否显示动画控件
+            baseLayerPicker: false, //是否显示图层选择控件
+            geocoder: false, //是否显示地名查找控件
+            timeline: false, //是否显示时间线控件
+            navigationHelpButton: false, //是否显示帮助信息控件
+            infoBox: false, //是否显示点击要素之后显示的信息
+            sceneModePicker: false, //是否显示3D/2D选择器
+            vrButton: false, //双屏模式,默认不显示false
+            fullscreenButton: false,
+            selectionIndicator: false,
+            shadows: true,
+            shouldAnimate: true,
             terrainProvider: Cesium.createWorldTerrain(),
         })
         const handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas)
 
         // Add Cesium OSM buildings to the scene as our example 3D Tileset.
-        const osmBuildingsTileset = Cesium.createOsmBuildings()
+        osmBuildingsTileset = Cesium.createOsmBuildings()
         viewer.scene.primitives.add(osmBuildingsTileset)
 
         // Set the initial camera to look at Seattle
@@ -29,34 +44,35 @@ const Vrbuilding: React.FC = () => {
             },
         })
 
-        const colorByMaterial = () => {
-            osmBuildingsTileset.style = new Cesium.Cesium3DTileStyle({
-                defines: {
-                    material: "${feature['building:material']}",
-                },
-                color: {
-                    conditions: [
-                        ['${material} === null', "color('white')"],
-                        ["${material} === 'glass'", "color('skyblue', 0.5)"],
-                        ["${material} === 'concrete'", "color('grey')"],
-                        ["${material} === 'brick'", "color('indianred')"],
-                        ["${material} === 'stone'", "color('lightslategrey')"],
-                        ["${material} === 'metal'", "color('lightgrey')"],
-                        ["${material} === 'steel'", "color('lightsteelblue')"],
-                        ['true', "color('white')"], // This is the else case
-                    ],
-                },
-            })
-        }
         colorByMaterial()
+        createModel('glb/Air.glb', 500)
+    }
+
+    const colorByMaterial = () => {
+        osmBuildingsTileset.style = new Cesium.Cesium3DTileStyle({
+            defines: {
+                material: "${feature['building:material']}",
+            },
+            color: {
+                conditions: [
+                    ['${material} === null', "color('white')"],
+                    ["${material} === 'glass'", "color('skyblue', 0.5)"],
+                    ["${material} === 'concrete'", "color('grey')"],
+                    ["${material} === 'brick'", "color('indianred')"],
+                    ["${material} === 'stone'", "color('lightslategrey')"],
+                    ["${material} === 'metal'", "color('lightgrey')"],
+                    ["${material} === 'steel'", "color('lightsteelblue')"],
+                    ['true', "color('white')"], // This is the else case
+                ],
+            },
+        })
     }
     // "../SampleData/models/CesiumAir/Cesium_Air.glb",
     // 5000.0
     const createModel = (url: string, height: number) => {
         viewer.entities.removeAll()
-
-        const position = Cesium.Cartesian3.fromDegrees(-123.0744619, 44.0503706, height)
-        const heading = Cesium.Math.toRadians(135)
+        const position = Cesium.Cartesian3.fromDegrees(-122.3472, 47.518, height)
+        const heading = Cesium.Math.toRadians(-90)
         const pitch = 0
         const roll = 0
         const hpr = new Cesium.HeadingPitchRoll(heading, pitch, roll)
@@ -75,9 +91,40 @@ const Vrbuilding: React.FC = () => {
         viewer.trackedEntity = entity
     }
 
+    const colorChange = () => {
+        osmBuildingsTileset.style = new Cesium.Cesium3DTileStyle({
+            color: {
+                conditions: [
+                    [
+                        "${feature['building']} === 'apartments' || ${feature['building']} === 'residential'",
+                        "color('cyan', 0.9)",
+                    ],
+                    [true, "color('white')"],
+                ],
+            },
+        })
+    }
+    const handleChange = (value: string) => {
+        if (value == '1') {
+            colorByMaterial()
+        } else if (value == '2') {
+            colorChange()
+        }
+    }
     return (
         <Panel>
-            <div id="buildingContainer"></div>
+            <div>
+                <div id="buildingContainer"></div>
+                <Select
+                    defaultValue="1"
+                    style={{ width: 120 }}
+                    onChange={handleChange}
+                    className="style_select"
+                >
+                    <Option value="1">默认颜色</Option>
+                    <Option value="2">换肤功能</Option>
+                </Select>
+            </div>
         </Panel>
     )
 }
